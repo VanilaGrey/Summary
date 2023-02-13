473a06e2f2ea61b192b1541de42efb2ae07caab9
VanilaGrey <VanilaGrey@users.noreply.github.com>
2023-02-13 16:48:43 +0000

    deploy: 387420798d67a3244e10f9b6c1b0469490e050b7

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,1 @@
-(()=>{var a=(n,t,e=0)=>{let r=n.split(""),o=0,i=setInterval(()=>{t.textContent+=r[o],o++,o===r.length&&clearInterval(i)},e)};var l=n=>t=>{let{intervalContent:e}=t.dataset;t.hidden=!1,a(e,t,n)};var s=100;document.querySelectorAll("[data-interval-content]").forEach(l(s));})();
+(()=>{var a=(n,t,e=0)=>{let r=n.split(""),o=0,i=setInterval(()=>{t.textContent+=r[o],o++,o===r.length&&clearInterval(i)},e)};var l=n=>t=>{let{intervalContent:e}=t.dataset;t.hidden=!1,a(e,t,n)};var s=60;document.querySelectorAll("[data-interval-content]").forEach(l(s));})();
